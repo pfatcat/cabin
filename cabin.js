98f0ff4b7e02468ed57973d617d4cedeceaae67c
Chuck Pfaff <chuck@fortyau.com>
2021-02-06 17:10:28 -0500

    re-auth working

--- cabin.js
+++ cabin.js
@@ -3,27 +3,38 @@ const request = require('request');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  getEnergyInfo(res)
+app.get('/info', (req, res) => { 
+    getData(res);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-function get(url, callback){
-    request(url, function (error, response, body) {
-        // console.error('error:', error); // Print the error if one occurred
-        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
-        callback(body);
+function getData(res){
+    getEnergyInfo(function(data){
+        res.send(data);
     });
 }
 
+function get(url, callback){
+    request(url, function (error, response, body) {
+        callback(error, response, body);
+    });
+}
+
+function post(url, body, callback){
+    request({
+        url: url,
+        method: "POST",
+        json: body
+    }, 
+    function (error, response, body) {
+        callback(error, response, body);
+    })
+}
 
 const config = require('config');
-firebaseAuthToken = config.firebaseAuthToken;
-console.dir(firebaseAuthToken);
 
 function httpGetAsync(url, callback)
 {
@@ -37,19 +48,19 @@ function httpGetAsync(url, callback)
     xmlHttp.send(null);
 }
 
-function httpPostAsync(url, body, callback)
-{
-    let json = JSON.stringify(body);
+// function httpPostAsync(url, body, callback)
+// {
+//     let json = JSON.stringify(body);
     
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4)
-            callback(xmlHttp);
-    }
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function() { 
+//         if (xmlHttp.readyState == 4)
+//             callback(xmlHttp);
+//     }
 
-    xmlHttp.open("POST", url, true); // true for asynchronous 
-    xmlHttp.send(json);
-}
+//     xmlHttp.open("POST", url, true); // true for asynchronous 
+//     xmlHttp.send(json);
+// }
 
 const getWeatherInfo = function(){
    const url="https://api.ambientweather.net/v1/devices/mac-address?apiKey=api+key&applicationKey=app_key&limit=1";
@@ -62,21 +73,21 @@ const getWeatherInfo = function(){
 
 };
 
-const getEnergyInfo = function(res){
+const getEnergyInfo = function(cb){
    const url="https://cabin-3bebb.firebaseio.com/solar_stats/2021-02-06.json?auth=";
-   let full_url = url + firebaseAuthToken;
+   let full_url = url + config.firebaseAuthToken;
 
-    get(full_url, function(response){
-        if(response.status === 401){
-             firebaseAuth(function(firebaseAuthToken){
-                full_url = url + firebaseAuthToken;
-                httpGetAsync(full_url, function(wat2){
-                    parseFirebaseResponse(wat2);
+    get(full_url, function(error, response, body){
+        if(response.statusCode === 401){
+             firebaseAuth(function(){
+                full_url = url + config.firebaseAuthToken;
+                get(full_url, function(error, response, body){
+                    cb(parseFirebaseResponse(body));
                 });
              });
         }
         else{
-            res.send(parseFirebaseResponse(response));
+            cb(parseFirebaseResponse(body));
         }  
      });
  
@@ -90,15 +101,18 @@ const getEnergyInfo = function(res){
 
  const firebaseAuth = function(callback){
     const creds = {
-        email: "user",
-        password: "pass",
+        email: config.firebaseUsername,
+        password: config.firebasePassword,
         returnSecureToken: true
       };
 
-    authUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=web_api";
+    //authUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=web_api";
+    authUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${config.firebaseAPIKey}`;
 
-    httpPostAsync(authUrl, creds, function(wat){
-        callback(JSON.parse(wat.response).idToken);
+    post(authUrl, creds, function(error, response, body){
+        // authToken = body.idToken;
+        config.firebaseAuthToken = body.idToken;
+        callback();
     });
  };
 
